e31a83b6f20e4985c54c412385e9d64354824609
alphaibrahima <alphaibrahimas95@gmail.com>
2021-06-24 20:01:16 +0000

    firt commit tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      spacing: {
+        15: "3.875rem"
+      },
+      colors: {
+        sombre: "#24292e"
+      }
+
+    },
+
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
